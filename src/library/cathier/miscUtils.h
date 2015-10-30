brainvisa/aims-til

commit 27e8e00e46be82fa182e2ce895bc41942b982e9c

Tag obsolete items with the 'deprecated' GCC annotation
This causes GCC to emit a warning when these items are used, and thus helps to eliminate their usage


git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-til/branches/bug_fix@69910 613412ce-1361-4b7a-93bf-a3c75af1bae4

src/library/cathier/miscUtils.h
@@ -296,6 +296,7 @@ namespace til
   // TODO: change this: remove res = T()
   template < typename TRes, typename TCollection >
   void centroid(const TCollection & c, TRes & res)
+    __attribute__((__deprecated__("use mean() instead")))
   {
     mean(c.begin(), c.end(), res);
   }

src/library/til/numeric_array_tools.h
@@ -18,7 +18,7 @@ namespace til
 #ifndef NDEBUG
   newSize
 #endif
-   )
+  ) __attribute__((__deprecated__))
   {
     assert(newSize == D);
   }

commit 7a0ff675edaea2e651625b0b5bb286c34641bd4e

Fix compilation with GCC
Bug introduced in r69910, sorry about that


git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-til/branches/bug_fix@69914 613412ce-1361-4b7a-93bf-a3c75af1bae4

src/library/cathier/miscUtils.h
@@ -296,7 +296,10 @@ namespace til
   // TODO: change this: remove res = T()
   template < typename TRes, typename TCollection >
   void centroid(const TCollection & c, TRes & res)
-    __attribute__((__deprecated__("use mean() instead")))
+    __attribute__((__deprecated__("use mean() instead")));
+
+  template < typename TRes, typename TCollection >
+  void centroid(const TCollection & c, TRes & res)
   {
     mean(c.begin(), c.end(), res);
   }

src/library/til/numeric_array_tools.h
@@ -14,11 +14,15 @@ namespace til
   // won't affect a.size()...
   //TODO: I think this crap is completely obsolete, right? I hope so.
   template < typename T, std::size_t D >
+  inline void resize(numeric_array<T,D> &, std::size_t newSize)
+    __attribute__((__deprecated__));
+
+  template < typename T, std::size_t D >
   inline void resize(numeric_array<T,D> &, std::size_t
 #ifndef NDEBUG
   newSize
 #endif
-  ) __attribute__((__deprecated__))
+  )
   {
     assert(newSize == D);
   }