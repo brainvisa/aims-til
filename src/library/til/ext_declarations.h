254c54e035d57b6606ac5502689c28aee434bd11
denisri <denis.riviere.gm@gmail.com>
2015-11-15 22:37:41 +0000

    fixed compilation with gcc 5.2
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-til/branches/bug_fix@70137 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/library/til/ext_declarations.h
+++ src/library/til/ext_declarations.h
@@ -1,6 +1,11 @@
 #ifndef DECLARATIONS_EXTERNAL_H_
 #define DECLARATIONS_EXTERNAL_H_
 
+#ifndef _GLIBCXX_BEGIN_NAMESPACE_CXX11
+#define _GLIBCXX_BEGIN_NAMESPACE_CXX11
+#define _GLIBCXX_END_NAMESPACE_CXX11
+#endif
+
 /// \file Forward declarations of external classes.
 
 namespace boost
@@ -11,11 +16,15 @@ namespace boost
 
 namespace std
 {
+  // in gcc 5.2 list is defined in std::__cxx11 namespace
+_GLIBCXX_BEGIN_NAMESPACE_CXX11
   template < typename T, typename TAlloc >        class list;
+_GLIBCXX_END_NAMESPACE_CXX11
+
   template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
                                                   class map;
   template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
-                                                  class multimap;  
+                                                  class multimap;
   template < typename TKey, typename TCompare, typename TAlloc >
                                                   class set;
   template < typename T, typename TAlloc >        class vector;
