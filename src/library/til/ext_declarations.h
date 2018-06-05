028a4fe32bbf1c2b15435c347285f77f1897d0c2
denisri <denis.riviere.gm@gmail.com>
2018-06-05 09:53:06 +0000

    removed forward declarations for STL classes, which do not always work
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-til/branches/bug_fix@81809 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/library/til/ext_declarations.h
+++ src/library/til/ext_declarations.h
@@ -1,10 +1,18 @@
 #ifndef DECLARATIONS_EXTERNAL_H_
 #define DECLARATIONS_EXTERNAL_H_
 
-#ifndef _GLIBCXX_BEGIN_NAMESPACE_CXX11
-#define _GLIBCXX_BEGIN_NAMESPACE_CXX11
-#define _GLIBCXX_END_NAMESPACE_CXX11
-#endif
+// #ifndef _GLIBCXX_BEGIN_NAMESPACE_CXX11
+// #define _GLIBCXX_BEGIN_NAMESPACE_CXX11
+// #define _GLIBCXX_END_NAMESPACE_CXX11
+// #endif
+
+// forward declarations for std:: templates is dangerous and can lead to
+// ambiguities with some compilers. Let's not do this.
+
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
 
 /// \file Forward declarations of external classes.
 
@@ -14,20 +22,20 @@ namespace boost
   template < typename T >                         class shared_ptr;
 }
 
-namespace std
-{
-  // in gcc 5.2 list is defined in std::__cxx11 namespace
-_GLIBCXX_BEGIN_NAMESPACE_CXX11
-  template < typename T, typename TAlloc >        class list;
-_GLIBCXX_END_NAMESPACE_CXX11
-
-  template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
-                                                  class map;
-  template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
-                                                  class multimap;
-  template < typename TKey, typename TCompare, typename TAlloc >
-                                                  class set;
-  template < typename T, typename TAlloc >        class vector;
-}
+// namespace std
+// {
+//   // in gcc 5.2 list is defined in std::__cxx11 namespace
+// _GLIBCXX_BEGIN_NAMESPACE_CXX11
+//   template < typename T, typename TAlloc >        class list;
+// _GLIBCXX_END_NAMESPACE_CXX11
+//
+//   template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
+//                                                   class map;
+//   template < typename TKey, typename TValue, typename TCompare, typename TAlloc >
+//                                                   class multimap;
+//   template < typename TKey, typename TCompare, typename TAlloc >
+//                                                   class set;
+//   template < typename T, typename TAlloc >        class vector;
+// }
 
 #endif /*DECLARATIONS_EXTERNAL_H_*/
